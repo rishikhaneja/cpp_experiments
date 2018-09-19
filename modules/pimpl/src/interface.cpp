46d08b6a06cce123af053db4381b186bf105bc38
Rishi Khaneja <rishikhaneja@gmail.com>
2018-09-19 21:19:42 +0200

    another project, and some clean up

--- modules/pimpl/src/interface.cpp
+++ modules/pimpl/src/interface.cpp
@@ -1,12 +1,13 @@
 #include "interface.hpp"
 
 #include <iostream>
+#include <memory>
 
 struct interface::impl {
   void function() { std::cout << "pimpl" << std::endl; }
 };
 
-interface::interface() : m_impl(new impl) {}
+interface::interface() : m_impl(std::make_unique<impl>()) {}
 
 interface::~interface() = default;
 
