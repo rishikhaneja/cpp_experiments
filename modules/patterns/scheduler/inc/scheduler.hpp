f82fc72bda3c7fa923b93589772a07c83b19912c
Rishi Khaneja <rishikhaneja@gmail.com>
2019-11-30 15:28:59 +0200

    final scheduler

--- modules/patterns/scheduler/inc/scheduler.hpp
+++ modules/patterns/scheduler/inc/scheduler.hpp
@@ -1,21 +1,172 @@
 #pragma once
 
+#include <algorithm>
+#include <cassert>
 #include <functional>
+#include <iostream>
+#include <iterator>
 #include <memory>
+#include <optional>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <variant>
+#include <vector>
+
+using namespace std;
+
+template <class... Ts>
+struct overloaded : Ts... {
+    using Ts::operator()...;
+};
+template <class... Ts>
+overloaded(Ts...)->overloaded<Ts...>;
+
+template <typename T>
+struct recursive_wrapper {
+    recursive_wrapper(T t_) : t(new T(std::move(t_))) {}
+    operator const T&() const { return *t; }
+    operator T&() { return *t; }
+    unique_ptr<T> t;
+};
 
 class Scheduler {
    public:
-    using Task = std::function<void(void)>;
+    using Body = function<void(void)>;
+    struct LeafTask {
+        string name;
+        Body body;
+    };
+    struct CompositeTask;
+    using CompositeTaskWrapper = recursive_wrapper<CompositeTask>;
+    using Task = variant<LeafTask, CompositeTaskWrapper>;
 
-    void add(string_view name, Task task);
-    void addBefore(string_view name, Task task, string_view otherTask);
-    void addAfter(string_view name, Task task, string_view otherTask);
-    void execute();
+    static string getName(const Task& t) {
+        return visit(
+            overloaded{[](const CompositeTask& c) { return c.getName(); },
+                       [](const LeafTask& l) { return l.name; }},
+            t);
+    }
 
-    Scheduler();
-    ~Scheduler();
+    class CompositeTask {
+       public:
+        string getName() const { return m_name; }
+
+        void pushBack(Task task) {
+            auto name = Scheduler::getName(task);
+            assert(m_registry.find(name) == m_registry.end());
+            m_tasks.push_back(move(task));
+            m_registry[move(name)] = m_tasks.size() - 1;
+        }
+
+        void insertBefore(Task task, string_view otherTask) {
+            auto name = Scheduler::getName(task);
+            auto otherIndex = findTaskIndex(otherTask);
+            assert(otherIndex);
+            m_tasks.insert(m_tasks.begin() + *otherIndex, move(task));
+            updateIndicesAfter(*otherIndex);
+        }
+
+        void insertAfter(Task task, string_view otherTask) {
+            auto name = Scheduler::getName(task);
+            auto otherIndex = findTaskIndex(otherTask);
+            assert(otherIndex);
+            m_tasks.insert(m_tasks.begin() + (*otherIndex + 1), move(task));
+            updateIndicesAfter(*otherIndex + 1);
+        }
+
+        Task* findTask(string_view name) {
+            auto index = findTaskIndex(name);
+            if (index) {
+                return &(m_tasks[*index]);
+            } else {
+                return nullptr;
+            }
+        }
+
+        CompositeTask* findCompositeTask(string_view name) {
+            auto task = findTask(name);
+            if (task) {
+                auto compositeTaskWrapper = get_if<CompositeTaskWrapper>(task);
+                if (compositeTaskWrapper) {
+                    return &static_cast<CompositeTask&>(*compositeTaskWrapper);
+                } else {
+                    return nullptr;
+                }
+            } else {
+                return nullptr;
+            }
+        }
+
+        template <typename E>
+        void execute(E&& executor = DefaultExecutor{}) {
+            for (auto& t : m_tasks) {
+                visit(forward<E>(executor), t);
+            }
+        }
+
+        CompositeTask(string name) : m_name(name) {}
+        ~CompositeTask() = default;
+        CompositeTask(const CompositeTask&) = delete;
+        CompositeTask& operator=(const CompositeTask&) = delete;
+        CompositeTask(CompositeTask&&) = default;
+        CompositeTask& operator=(CompositeTask&&) = default;
+
+       private:
+        void updateIndicesAfter(size_t index) {
+            for (; index != m_tasks.size(); ++index) {
+                auto& t = m_tasks[index];
+                m_registry[Scheduler::getName(t)] = index;
+            }
+        }
+
+        optional<size_t> findTaskIndex(string_view name) {
+            auto it = m_registry.find(string(name));
+            if (it != m_registry.end()) {
+                return it->second;
+            } else {
+                return {};
+            }
+        }
+
+        string m_name;
+        vector<Task> m_tasks;
+        unordered_map<string, size_t> m_registry;
+    };
+
+    void pushBack(Task task) { m_mainTask.pushBack(move(task)); }
+
+    void insertBefore(Task task, string_view otherTask) {
+        m_mainTask.insertBefore(move(task), otherTask);
+    }
+
+    void insertAfter(Task task, string_view otherTask) {
+        m_mainTask.insertAfter(move(task), otherTask);
+    }
+
+    Task* findTask(string_view name) { return m_mainTask.findTask(name); }
+
+    CompositeTask* findCompositeTask(string_view name) {
+        return m_mainTask.findCompositeTask(name);
+    }
+
+    struct DefaultExecutor {
+        void operator()(LeafTask& l) { l.body(); }
+        void operator()(CompositeTask& c) { c.execute(*this); }
+    };
+
+    template <typename E = DefaultExecutor>
+    void execute(E&& executor = DefaultExecutor{}) {
+        m_mainTask.execute(forward<E>(executor));
+    }
+
+    Scheduler() = default;
+    ~Scheduler() = default;
+    Scheduler(const Scheduler&) = delete;
+    Scheduler& operator=(const Scheduler&) = delete;
+    Scheduler(Scheduler&&) = default;
+    Scheduler& operator=(Scheduler&&) = default;
 
    private:
-    struct Impl;
-    std::unique_ptr<Impl> m_impl;
+    CompositeTask m_mainTask{"main"};
 };
--- modules/patterns/scheduler/src/test.cpp
+++ modules/patterns/scheduler/src/test.cpp
@@ -2,7 +2,6 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-
 #include "scheduler.hpp"
 
 using namespace std;
@@ -10,36 +9,69 @@ using namespace testing;
 
 struct Main {
     MOCK_METHOD0(update, void(void));
-    MOCK_METHOD0(render, void(void));
+    MOCK_METHOD0(render1, void(void));
+    MOCK_METHOD0(render2, void(void));
+};
+
+struct MainExtension {
+    MOCK_METHOD0(preUpdate, void(void));
+    MOCK_METHOD0(postUpdate, void(void));
+    MOCK_METHOD0(preRender2, void(void));
+    MOCK_METHOD0(postRender2, void(void));
+};
+
+using Composite = Scheduler::CompositeTask;
+using Leaf = Scheduler::LeafTask;
+
+struct CallOrderPrinter {
+    string indent;
+    void operator()(Leaf& l) { cout << indent << l.name << "\n"; }
+    void operator()(Composite& c) {
+        cout << indent << c.getName() << "\n";
+        c.execute(CallOrderPrinter{indent + indent});
+    }
 };
 
 TEST(scheduler, basic) {
     Main m;
     Scheduler s;
 
-    s.add([&m] { m.update(); });
-    s.add([&m] { m.render(); });
+    s.pushBack(Leaf{"update", [&m] { m.update(); }});
 
-    EXPECT_CALL(m, update()).Times(1);
-    EXPECT_CALL(m, render()).Times(1);
+    {
+        Composite render{"render"};
+        render.pushBack(Leaf{"render1", [&m] { m.render1(); }});
+        render.pushBack(Leaf{"render2", [&m] { m.render2(); }});
+        s.pushBack(move(render));
+    }
+
+    MainExtension me;
+
+    s.insertBefore(Leaf{"preUpdate", [&me] { me.preUpdate(); }}, "update");
+    s.insertAfter(Leaf{"postUpdate", [&me] { me.postUpdate(); }}, "update");
+
+    {
+        auto render = s.findCompositeTask("render");
+        render->insertBefore(Leaf{"preRender2", [&me] { me.preRender2(); }},
+                             "render2");
+        render->insertAfter(Leaf{"postRender2", [&me] { me.postRender2(); }},
+                            "render2");
+    }
+
+    {
+        InSequence seq;
+
+        EXPECT_CALL(me, preUpdate()).Times(1);
+        EXPECT_CALL(m, update()).Times(1);
+        EXPECT_CALL(me, postUpdate()).Times(1);
+        EXPECT_CALL(m, render1()).Times(1);
+        EXPECT_CALL(me, preRender2()).Times(1);
+        EXPECT_CALL(m, render2()).Times(1);
+        EXPECT_CALL(me, postRender2()).Times(1);
+    }
 
     s.execute();
-}
 
-struct MainExtension {
-    MOCK_METHOD0(preUpdate, void(void));
-    MOCK_METHOD0(postUpdate, void(void));
-};
-
-TEST(scheduler, extension) {
-    Main m;
-    Scheduler s;
-
-    s.add([&m] { m.update(); });
-    s.add([&m] { m.render(); });
-
-    EXPECT_CALL(m, update()).Times(1);
-    EXPECT_CALL(m, render()).Times(1);
-
-    s.execute();
+    cout << "Call Order:\n";
+    s.execute(CallOrderPrinter{"  "});
 }
