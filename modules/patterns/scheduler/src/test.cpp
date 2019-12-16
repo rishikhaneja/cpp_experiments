b4ed991343dbd7c311201e4d9d382cc728d338d4
Rishi Khaneja <rishikhaneja@gmail.com>
2019-12-17 00:56:26 +0200

    proto

--- modules/patterns/scheduler/src/test.cpp
+++ modules/patterns/scheduler/src/test.cpp
@@ -2,55 +2,113 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "scheduler.hpp"
+//#include "scheduler.hpp"
+
+#include <functional>
+#include <set>
+#include <string>
+#include <vector>
 
 using namespace std;
 using namespace testing;
 
-using Priority = Scheduler::Priority;
+struct Scheduler {
+    struct Task {
+        enum class Mode { OneShot, Recurring };
+        using Body = function<void(void)>;
 
-namespace {
-Priority update;
-Priority layout;
-Priority render;
+        string name;
+        Body body;
+        Mode mode = Mode::Recurring;
+    };
 
-struct Main {
-    MOCK_METHOD0(update, void(void));
-    MOCK_METHOD0(layout, void(void));
-    MOCK_METHOD0(render, void(void));
+    using TaskExecutor = function<void(const Task&)>;
+    using TaskList = vector<Task>;
+
+    using Stage = TaskList;
+
+    void executeAll() {
+        executeAll([this](const auto& task) { task.body(); });
+    }
+    void executeAll(TaskExecutor executor) {
+        for (auto& stage : stages) {
+            stage->erase(remove_if(begin(*stage),
+                                   end(*stage),
+                                   [&executor](auto& task) {
+                                       executor(task);
+                                       return task.mode == Task::Mode::OneShot;
+                                   }),
+                         end(*stage));
+            if (!proceed) {
+                return;
+            }
+        }
+    }
+    vector<Stage*> stages;
+    bool proceed = true;
 };
 
-struct MainExtension {
-    MOCK_METHOD0(preUpdate, void(void));
-    MOCK_METHOD0(postUpdate, void(void));
-    MOCK_METHOD0(final, void(void));
+class Application {
+   public:
+    static Scheduler::Stage UpdateStage;
+    static Scheduler::Stage LayoutStage;
+    static Scheduler::Stage RenderStage;
+    void init() {
+        // default stages
+        m_scheduler.stages = {&UpdateStage, &LayoutStage, &RenderStage};
+
+        // default tasks
+        UpdateStage.push_back(
+            {"update", [this] { update(); }, Scheduler::Task::Mode::Recurring});
+        LayoutStage.push_back(
+            {"layout", [this] { layout(); }, Scheduler::Task::Mode::Recurring});
+        RenderStage.push_back(
+            {"render", [this] { render(); }, Scheduler::Task::Mode::Recurring});
+
+        onInit();
+    }
+    void main() {
+        while (!m_quit) {
+            m_scheduler.executeAll();
+        }
+    }
+    void update() {
+        // do something
+        if (m_dirty) {
+            m_scheduler.proceed = true;
+        }
+    }
+    void layout() {}
+    void render() {}
+    void quit() {}
+
+   protected:
+    Scheduler m_scheduler;
+    bool m_quit = false;
+
+   private:
+    virtual void onInit(){};
+
+    bool m_dirty = false;
 };
 
-}  // namespace
+Scheduler::Stage Application::UpdateStage;
+Scheduler::Stage Application::LayoutStage;
+Scheduler::Stage Application::RenderStage;
+
+class MyApplication : public Application {
+    virtual void onInit() override {
+        RenderStage.insert(end(RenderStage),
+                           {"postRender",
+                            [this] { postRender(); },
+                            Scheduler::Task::Mode::Recurring});
+    }
+    void preUpdate() {}
+    void postRender() { quit(); }
+};
 
 TEST(scheduler, basic) {
-    Main m;
-    update.at = {{"update", [&m]() { m.update(); }}};
-    layout.at = {{"layout", [&m]() { m.layout(); }}};
-    render.at = {{"render", [&m]() { m.render(); }}};
-
-    MainExtension me;
-
-    Priority final(render);
-
-    update.before.push_back({"preUpdate", [&me]() { me.preUpdate(); }});
-    update.after.push_back({"postUpdate", [&me]() { me.postUpdate(); }});
-    final.at = {{"final", [&me]() { me.final(); }}};
-
-    {
-        InSequence seq;
-        EXPECT_CALL(me, preUpdate());
-        EXPECT_CALL(m, update());
-        EXPECT_CALL(me, postUpdate());
-        EXPECT_CALL(m, layout());
-        EXPECT_CALL(m, render());
-        EXPECT_CALL(me, final());
-    }
-
-    Scheduler::execute();
+    MyApplication app;
+    app.init();
+    app.main();
 }
